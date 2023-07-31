e17b1b6343e63491128affeb7e1b16e090e793af
Bashir Rashid <karche475b@gmail.com>
2023-07-31 17:37:08 +0300

    This is the solution of LARGEST-OF-TWO numbers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let a = 10
+let b = 20
+
+if(a > b)
+{
+     console.log(`A is greater than B`)
+}
+else if(a < b)
+{
+     console.log(`B is greater than A`)
+}
